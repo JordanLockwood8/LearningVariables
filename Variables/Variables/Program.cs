2cc4d2669c3136fdbedb910f8268ea1e711e2398
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-01 11:26:35 -0600

    first commit, learning variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿string tortoiseName = "Squish";
+int tortAge = 5;
+char L = '6';//L Stands for length
+bool real = true;
+double tortWidth = 4.0;
+decimal tortHeight = 3.5m;
+Console.WriteLine($"My tortoises name is {tortoiseName}, He is {tortAge} years old");
